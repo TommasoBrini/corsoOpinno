ecd64b90f60d412d1bb49febb339825fabf45cb1
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-06 10:58:04 +0200

    Solved ex Floppy

---
+++ Floppy/src/Floppy.java
@@ -0,0 +1,22 @@
+public class Floppy {
+    private final static byte CAPIENZA = (byte) 1.474560;
+
+    /**
+     * posiziona la testina in posizione k
+     * @param k
+     */
+    public void posizionaTestina(int k){
+
+    }
+
+    /**
+     * legge i prossimi x byte
+     * @param x
+     */
+    public void leggi(int x){
+
+    }
+
+    public void scrivi()
+
+}
