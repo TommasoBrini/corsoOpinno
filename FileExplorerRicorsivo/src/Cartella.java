3dd41a0384e0175625e75e3cbd12dab7405aedf3
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-08 13:13:07 +0200

    Update project FileExplorerRicorsivo

--- FileExplorerRicorsivo/src/Cartella.java
+++ FileExplorerRicorsivo/src/Cartella.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cartella {
     Path path;
@@ -9,26 +11,84 @@ public class Cartella {
     }
 
     public void stampa(){
-        System.out.println("" + path);
+        System.out.println(path.toString());
+        File file = new File(path.toString());
+        System.out.println(file.listFiles());
     }
 
-    public String cercaFile(String nomeFile){
-        StringBuilder sb = new StringBuilder();
-        cercaFile(new File(path.toString()), nomeFile, sb);
-        return sb.toString();
+    public Path getPath() {
+        return path;
     }
 
-    private void cercaFile(File cartella, String nomeFile, StringBuilder sb) {
-        File[] files = cartella.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    cercaFile(file, nomeFile, sb);
-                } else if (file.getName().equals(nomeFile)) {
-                    sb.append(file.getAbsolutePath()).append("\n");
+
+    /**
+     * Cerca file all'interno di una cartella
+     * @param directory cartella madre dove cercare il file
+     * @param nomeFile nome del file da cercare
+     * @return strina del percorso del file trovato
+     */
+    public String cercaFile(File directory, String nomeFile) {
+        // creare un array con tutti i file/cartelle nella cartella directory
+        File[] files = directory.listFiles();
+
+        // cicla per ogni file/cartella che trova in files
+        for(File file : files){
+            if(file.isDirectory()){
+                //se file è una cartella, chiamata ricorsiva passando questa cartella (file) e il nome del file da cercare
+                String res = cercaFile(file, nomeFile);
+                if(res != null){
+                    // se trova il file, restituire res
+                    return res;
+                }
+            } else {
+                if(file.getName().equals(nomeFile)){
+                    //Se il nome del file da trovare è uguale al nome di file, ritornare il percorso come stringa
+                    return "Path File: " + file.getPath();
                 }
             }
         }
+        return null;
+    }
+
+    /**
+     * Cercare ricorsivamente un file all'interno di una sottocartella
+     * @param sottocartella sottocartella in cui cercare
+     * @param nomeFile nome del file da cercare
+     * @return la stringa del percorso del file trovato
+     */
+    public String cercaFileInSottoCartella(String sottocartella, String nomeFile){
+        //creo la cartella a partite dalla stringa sottocartella, contenente il nome
+        File subDirectory = new File(path.toString(), sottocartella);
+        //chiamo cercaFile passandogli come cartella principale la sottocartella selezionata
+        return cercaFile(subDirectory, nomeFile);
+    }
+
+    /**
+     * Cerca all'interno della cartella tutti i file con l'estensione specificata
+     * @param directory cartella madre in cui cercare
+     * @param estensione estensione specificata
+     * @return lista dei nomi dei file con estensione specificata trovati
+     */
+    public List<String> cercaFileByEstensione(File directory, String estensione){
+        //Creo uno nuovo array che conterrà i risultati finali
+        List<String> res = new ArrayList<>();
+        //Cerco tutti i file/cartelle all'interno della cartella directory
+        File[] files = directory.listFiles();
+        for (File f : files){
+            if(f.isDirectory()){
+                //Se f è una cartella, richiamo ricorsivamente passando f come nuova cartella madre
+                //assegno in una lista di appoggio i risultati trovati nella ricorsione
+                List<String> resRicorsivi = cercaFileByEstensione(f, estensione);
+                //aggiungo alla lista dei risultati tutti i risultati trovati nella ricorsione, salvati in resRicorsivi
+                res.addAll(resRicorsivi);
+            } else{
+                if(f.getName().endsWith(estensione)){
+                    //se f è un file con estensione specificata lo aggiungo a res
+                    res.add(f.getPath().toString());
+                }
+            }
+        }
+        return res;
     }
 
 }
--- FileExplorerRicorsivo/src/Main.java
+++ FileExplorerRicorsivo/src/Main.java
@@ -1,11 +1,16 @@
+import java.io.File;
 import java.nio.file.Paths;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Cartella cartella = new Cartella(Paths.get("FileExplorerRicorsivo", "test_folder"));
+        Cartella cartella = new Cartella(Paths.get("test_folder"));
 
-        System.out.println(cartella.cercaFile("file1.txt"));
+        System.out.println(cartella.cercaFile(new File(cartella.getPath().toString()), "file3.csv"));
+
+        System.out.println(cartella.cercaFileInSottoCartella("folder2", "file2.txt"));
+
+        System.out.println(cartella.cercaFileByEstensione(new File(cartella.getPath().toString()), ".txt"));
     }
 }
