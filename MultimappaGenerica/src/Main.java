08b56ef46cdfc619763a6206125c9413815c8ff0
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-15 12:59:00 +0200

    Update MultimappaGenerica

--- MultimappaGenerica/src/Main.java
+++ MultimappaGenerica/src/Main.java
@@ -1,23 +1,46 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
 
         MultimappaGenerica<String, String> multiMap = new MultimappaGenerica<>();
-        multiMap.put("CHIAVE", "VALORE");
+        multiMap.put("CHIAVE", "VALORE0S");
         multiMap.put("CHIAVE", "VALORE1");
         multiMap.put("CHIAVE", "VALORE2");
 
         MultimappaGenerica<String, String> multiMap2 = new MultimappaGenerica<>();
-        multiMap2.put("CHIAVE2", "valore");
-        multiMap2.put("CHIAVE2", "valore");
-        multiMap2.put("CHIAVE2", "valore");
+        multiMap2.put("CHIAVE2", "VALORE1");
+        multiMap2.put("CHIAVE2", "valore2");
+        multiMap2.put("CHIAVE2", "valore3");
+
+        System.out.println("*** AGGIUNGO MULTIMAP2 ***");
+        multiMap.putAll(multiMap2);
+        System.out.println(multiMap);
+
+        System.out.println("*** RIMUOVO MULTIMAP2 ***");
+        multiMap.removeAll(multiMap2);
+        System.out.println(multiMap);
+
+        System.out.println("*** VALORI CON CHIAVE 1 ***");
+        System.out.println(multiMap.get("CHIAVE"));
+
+        System.out.println("*** VALORI CON CHIAVE 1 CHE CONTENGONO S ***");
+        System.out.println(multiMap.get("CHIAVE", str -> str.contains("S")));
 
         multiMap.putAll(multiMap2);
 
+        System.out.println("*** VALORI CON DUPLICATI CONTENUTI NELLA MULTIMAPPA ***");
+        System.out.println(multiMap.values());
 
-        System.out.println(multiMap.get("CHIAVE2"));
+        System.out.println("*** VALORI NON DUPLICATI CONTENUTI NELLA MULTIMAPPA ***");
+        System.out.println(multiMap.valueSet());
+
+        System.out.println("*** TRASFORMO I VALORI IN INTERI ***");
+        System.out.println(multiMap.tranformToMultiMappa((key,value) -> value.length()));
+
+        System.out.println("*** AGGIUNGO ciao A OGNI VALORE ***");
+        multiMap.mapEach((key,value)-> value + "ciao");
+        System.out.println(multiMap);
+
     }
 }
--- MultimappaGenerica/src/MultimappaGenerica.java
+++ MultimappaGenerica/src/MultimappaGenerica.java
@@ -1,9 +1,11 @@
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class MultimappaGenerica<K, V> {
+public class MultimappaGenerica<K, V> implements Iterable<MultimappaGenerica.Elemento<K,V>>{
     Map<K, List<V>> map;
 
     public MultimappaGenerica() {
@@ -40,21 +42,50 @@ public class MultimappaGenerica<K, V> {
         return map.values().stream().flatMap(List::stream).collect(Collectors.toSet());
     }
 
-    public <Z> MultimappaGenerica<K, Z> transformToMultiMappa(BiFunction<K, V, Z> f) {
-        MultimappaGenerica<K, Z> transformedMultiMap = new MultimappaGenerica<>();
-        map.forEach((key, values) -> values.forEach(value -> transformedMultiMap.put(key, f.apply(key, value))));
-        return transformedMultiMap;
+    public <Z> MultimappaGenerica<K,Z> tranformToMultiMappa(BiFunction<K, V, Z> f){
+        MultimappaGenerica<K,Z> res = new MultimappaGenerica<>();
+        map.forEach((k,v) -> v.forEach(value -> res.put(k,f.apply(k,value))));
+        return res;
     }
 
-    public void mapEach(BiFunction<K, V, V> function) {
-        map.replaceAll((key, values) ->
-                values.stream()
-                        .map(value -> function.apply(key, value))
-                        .collect(Collectors.toList()));
+    public void mapEach(BiFunction<K,V,V> f){
+        map.replaceAll((k,v) -> v.stream()
+                .map(value -> f.apply(k,value))
+                .collect(Collectors.toList()));
     }
+
+
 
     @Override
     public String toString() {
         return "map=" + map;
     }
+
+    @Override
+    public Iterator<Elemento<K, V>> iterator() {
+        List<Elemento<K, V>> elements = new ArrayList<>();
+        map.forEach((key, values) ->
+                values.forEach(value -> elements.add(new Elemento<>(key, value))));
+        return elements.iterator();
+    }
+
+    public class Elemento<K,V>{
+        private final K key;
+        private final V value;
+
+        public Elemento(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+
 }
