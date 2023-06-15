2caff64e29d8a4ff46421475eccab5cad9b4558a
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-15 15:24:07 +0200

    Create scheletro exercise CivilRegistry

---
+++ CivilRegistry/src/Anagrafe.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anagrafe {
+    List<Persona> persone = new ArrayList<>();
+
+    public void addPersona(){
+
+    }
+
+    public void removePersona(String CF){
+
+    }
+
+    public Persona getPersona(String nome){
+        return null;
+    }
+
+    public List<Persona> getPersoneAnziane(){
+        return null;
+    }
+
+    public List<String> getIndirizzi(String nome){
+        return null;
+    }
+    
+}
---
+++ CivilRegistry/src/Persona.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Persona {
+
+    private String nome;
+    private String cognome;
+    private int eta;
+    private int CF;
+    private String indirizzo;
+
+}
