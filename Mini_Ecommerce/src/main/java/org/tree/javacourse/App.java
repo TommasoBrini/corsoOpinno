66eaac3a073057621e7816507113a09d56ece7df
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-23 14:49:06 +0200

    Update exercise Mini_Ecommerce

--- Mini_Ecommerce/src/main/java/org/tree/javacourse/App.java
+++ Mini_Ecommerce/src/main/java/org/tree/javacourse/App.java
@@ -1,7 +1,12 @@
 package org.tree.javacourse;
 
 import org.tree.javacourse.controller.ProdottoController;
+import org.tree.javacourse.dao.Dao;
+import org.tree.javacourse.dao.ProdottoDaoImpl;
+import org.tree.javacourse.model.Prodotto;
 import org.tree.javacourse.service.ProdottoService;
+
+import java.sql.SQLException;
 
 /**
  * Hello world!
@@ -10,12 +15,17 @@ import org.tree.javacourse.service.ProdottoService;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws SQLException {
         System.out.println( "Hello World!" );
-        ProdottoService prodottoService = new ProdottoService();
+        /*ProdottoService prodottoService = new ProdottoService();
 
         ProdottoController prodottoController = new ProdottoController();
-        prodottoController.startServices(prodottoService);
+        prodottoController.startServices(prodottoService);*/
+
+
+        Dao prodottoDAO = new ProdottoDaoImpl();
+
+        prodottoDAO.insert(new Prodotto("Cocaina", "Pura e legale", 50, 1000));
+
     }
 }
--- Mini_Ecommerce/src/main/java/org/tree/javacourse/dao/ProdottoDaoImpl.java
+++ Mini_Ecommerce/src/main/java/org/tree/javacourse/dao/ProdottoDaoImpl.java
@@ -1,7 +1,9 @@
 package org.tree.javacourse.dao;
 
+import org.tree.javacourse.connection_handler.ConnectionHandler;
 import org.tree.javacourse.model.Prodotto;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -9,8 +11,20 @@ import java.util.Optional;
 public class ProdottoDaoImpl implements Dao{
     @Override
     public boolean insert(Prodotto p) throws SQLException {
-        // QUUERY DI INSERIMENTO DATABASE
-        return false;
+        // QUERY DI INSERIMENTO DATABASE
+        String query = "INSERT INTO prodotto (nome, descrizione, quantitastock, prezzo) VALUES (?, ?, ?, ?)";
+        ConnectionHandler connectionHandler = ConnectionHandler.getInstance();
+
+        PreparedStatement ps = connectionHandler.getPreparedStatement(query);
+
+        ps.setString(1, p.getNome());
+        ps.setString(2, p.getDescrizione());
+        ps.setInt(3, p.getQuantitaStock());
+        ps.setDouble(4, p.getPrezzo());
+
+        ps.executeUpdate();
+
+        return true;
     }
 
     @Override
