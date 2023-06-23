d5e51e3144691ed2685ae0724784145951e1cb98
Tommaso <tommaso.brini@studio.unibo.it>
2023-06-23 15:37:42 +0200

    Solved exercise Mini_Ecommerce

--- Mini_Ecommerce/src/main/java/org/tree/javacourse/App.java
+++ Mini_Ecommerce/src/main/java/org/tree/javacourse/App.java
@@ -17,15 +17,20 @@ public class App
 {
     public static void main( String[] args ) throws SQLException {
         System.out.println( "Hello World!" );
-        /*ProdottoService prodottoService = new ProdottoService();
+        ProdottoService prodottoService = new ProdottoService();
 
         ProdottoController prodottoController = new ProdottoController();
-        prodottoController.startServices(prodottoService);*/
+        prodottoController.startServices(prodottoService);
 
 
-        Dao prodottoDAO = new ProdottoDaoImpl();
+        //Dao prodottoDAO = new ProdottoDaoImpl();
 
-        prodottoDAO.insert(new Prodotto("Cocaina", "Pura e legale", 50, 1000));
+        /*
+        prodottoDAO.insert(new Prodotto("Cocaina2", "Pura e legale", 50, 1000));
+        prodottoDAO.insert(new Prodotto("Cocaina3", "Pura e legale", 50, 1000));
+        prodottoDAO.insert(new Prodotto("Cocaina4", "Pura e legale", 50, 1000));
+        prodottoDAO.insert(new Prodotto("Cocaina5", "Pura e legale", 50, 1000));
+        prodottoDAO.insert(new Prodotto("Cocaina6", "Pura e legale", 50, 1000));*/
 
     }
 }
--- Mini_Ecommerce/src/main/java/org/tree/javacourse/dao/ProdottoDaoImpl.java
+++ Mini_Ecommerce/src/main/java/org/tree/javacourse/dao/ProdottoDaoImpl.java
@@ -4,7 +4,9 @@ import org.tree.javacourse.connection_handler.ConnectionHandler;
 import org.tree.javacourse.model.Prodotto;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,25 +31,61 @@ public class ProdottoDaoImpl implements Dao{
 
     @Override
     public boolean update(Prodotto p) throws SQLException {
-        // QUERY UPDATE DATABASE
-        return false;
+        String query = "UPDATE prodotto SET nome = ?, descrizione = ?, quantitastock = ?, prezzo = ? WHERE id = ?";
+
+        ConnectionHandler connectionHandler = ConnectionHandler.getInstance();
+
+        PreparedStatement ps = connectionHandler.getPreparedStatement(query);
+
+        ps.setString(1, p.getNome());
+        ps.setString(2, p.getDescrizione());
+        ps.setInt(3, p.getQuantitaStock());
+        ps.setDouble(3, p.getPrezzo());
+        ps.setInt(4, p.getId());
+
+        int updateCount = ps.executeUpdate();
+
+        return updateCount > 0;
     }
 
     @Override
     public boolean delete(int id) throws SQLException {
-        // QUERY DELETE DATABASE
-        return false;
+        String query = "DELETE FROM prodotto WHERE id = ?;";
+
+        ConnectionHandler ch = ConnectionHandler.getInstance();
+        PreparedStatement ps = ch.getPreparedStatement(query);
+
+        ps.setInt(1, id);
+        int deletedCount = ps.executeUpdate();
+        return deletedCount > 0;
     }
 
     @Override
     public Optional<Prodotto> get(int id) throws SQLException {
-        // QUERY SELECT DATABASE
-        return Optional.empty();
+        String query = "SELECT * FROM prodotto WHERE id = ?";
+
+        Optional<Prodotto> p = Optional.empty();
+
+        ConnectionHandler ch = ConnectionHandler.getInstance();
+        PreparedStatement ps = ch.getPreparedStatement(query);
+
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) p = Optional.of(new Prodotto(rs.getString("nome"), rs.getString("descrizione"), rs.getInt("quantitastock"), rs.getDouble("prezzo")));
+        return p;
     }
 
     @Override
     public List<Prodotto> getAll() throws SQLException {
-        // QUERY SELECT ALL DATABASE
-        return null;
+        String query = "SELECT * FROM prodotto WHERE id = ?";
+
+        List<Prodotto> p = new ArrayList<>();
+
+        ConnectionHandler ch = ConnectionHandler.getInstance();
+        PreparedStatement ps = ch.getPreparedStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) p.add(new Prodotto(rs.getString("nome"), rs.getString("descrizione"), rs.getInt("quantitastock"), rs.getDouble("prezzo")));
+        return p;
     }
 }
